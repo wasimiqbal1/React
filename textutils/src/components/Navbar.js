01801254be4789ffd35b307f4ecdf34ea3f9827e
wasimiqbal1 <wasim_iqbal@msn.com>
2021-09-12 01:34:50 +0500

    proptypes

--- textutils/src/components/Navbar.js
+++ textutils/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types'
 
 export default function Navbar(props) {
   return (
@@ -49,3 +50,6 @@ export default function Navbar(props) {
     </div>
   );
 }
+
+Navbar.propTypes = {title: PropTypes.string,
+                    aboutText:PropTypes.string}
