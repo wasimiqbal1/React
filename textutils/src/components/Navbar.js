42300acc124cf0001fbc2a9a998ed6c97a769f68
wasimiqbal1 <wasim_iqbal@msn.com>
2021-09-12 01:40:35 +0500

    Props

--- textutils/src/components/Navbar.js
+++ textutils/src/components/Navbar.js
@@ -51,5 +51,9 @@ export default function Navbar(props) {
   );
 }
 
-Navbar.propTypes = {title: PropTypes.string,
-                    aboutText:PropTypes.string}
+Navbar.propTypes = {title: PropTypes.string.isRequired,
+                    aboutText:PropTypes.string.isRequired}
+
+Navbar.defaultProps = {title: "Set Title here",
+    aboutText:"About Text here"};
+   
