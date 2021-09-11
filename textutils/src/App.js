880298f872b5af14e8221df41d8329a0bb12dd40
wasimiqbal1 <wasim_iqbal@msn.com>
2021-09-11 23:22:03 +0500

    React

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,28 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
+let name = "React";
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        header className = "App-header" >
-        <
-        img src = { logo }
-        className = "App-logo"
-        alt = "logo" / >
-        <
-        p >
-        Edit < code > src / App.js < /code> and save to reload. <
-        /p> <
-        a className = "App-link"
-        href = "https://reactjs.org"
-        target = "_blank"
-        rel = "noopener noreferrer" >
-        Learn React with Wasim <
-        /a> <
-        /header> <
-        /div>
+    return (
+        <>
+        <nav>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </nav>
+        <div className="container">
+            <h1>Hello {name}</h1>
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus totam libero porro maxime ipsum laboriosam explicabo nam perferendis, autem, eius fugiat! Deleniti ipsam ducimus expedita obcaecati id debitis, numquam vel!
+            </p>
+        </div>
+        </>
+ 
     );
 }
-
 export default App;
