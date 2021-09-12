8f4696150b77a5e5799a54c657627b0477279d30
wasimiqbal1 <wasim_iqbal@msn.com>
2021-09-12 17:30:38 +0500

    lowercase

--- textutils/src/components/Textform.js
+++ textutils/src/components/Textform.js
@@ -7,6 +7,12 @@ export default function Textform(props) {
         setText(newText);
     }
 
+    const handleloclick = ()=>{
+        // console.log("Lower Case was Clicked: " + text);
+        let newText = text.toLocaleLowerCase();
+        setText(newText);
+    }
+
     const handleOnChange = (event)=>{
         // console.log("On Changed");
         setText(event.target.value);
@@ -21,11 +27,16 @@ export default function Textform(props) {
             <div className="mb-3">
                 <textarea className="form-control" value = {text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary" onClick={handleUpclick}>Convert to Upper Case </button>
+            <button className="btn btn-primary mx-2" onClick={handleUpclick}>Convert to Upper Case </button>
+            <button className="btn btn-primary mx-2" onClick={handleloclick}>Convert to Lower Case </button>
         </div>
         <div className="container my-3">
-            <h1>Your Text Summary</h1>
+            <h2>Your Text Summary</h2>
             <p>{text.split(" ").length} word & {text.length} characters</p>
+            <p>{0.008 * text.split(" ").length} minutes read</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+
         </div>
         </>
     )
