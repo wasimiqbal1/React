c988259f57c05fe83370b0cb9ca0e8f9326f2de0
wasimiqbal1 <wasim_iqbal@msn.com>
2021-09-19 00:24:07 +0500

    accordion

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
-// import About from "./components/About";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
-import Textform from "./components/Textform";
+// import Textform from "./components/Textform";
 
 function App() {
   return (
     <>
       <Navbar title="TextUtils" aboutText="About us" />
       <div className="container my-3">
-      <Textform heading = "Enter the text to analyze"/>
-      {/* <About/> */}
+      {/* <Textform heading = "Enter the text to analyze"/> */}
+      <About/>
       </div>
     </>
   );
