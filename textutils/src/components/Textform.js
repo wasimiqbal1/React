d2380048772689fae2ab7b5cfa42faa62148245e
wasimiqbal1 <wasim_iqbal@msn.com>
2021-09-19 15:23:53 +0500

    Remove Extra Spaces func

--- textutils/src/components/Textform.js
+++ textutils/src/components/Textform.js
@@ -25,10 +25,14 @@ export default function Textform(props) {
     }
 
     const handleCopy = ()=>{
-        // console.log("Clear text was Clicked: " + text);
         let text = document.getElementById('myBox');
         text.select ();
         navigator.clipboard.writeText(text.value);
+    }
+
+    const handleExtraSpaces = ()=>{
+        let newtext = text.split(/[ ]+/);
+        setText(newtext.join(" "))
     }
 
     const [text, setText] = useState("Entre Text here");
@@ -44,6 +48,7 @@ export default function Textform(props) {
             <button className="btn btn-primary mx-2" onClick={handleloClick}>Convert to Lower Case </button>
             <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear</button>
             <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy Text</button>
+            <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
         </div>
         <div className="container my-3">
             <h2>Your Text Summary</h2>
