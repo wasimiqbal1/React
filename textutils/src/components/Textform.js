73c84597c3c27028ae2c7f4b6167108964f0e8cf
wasimiqbal1 <wasim_iqbal@msn.com>
2021-09-26 23:27:12 +0500

    dark mode

--- textutils/src/components/Textform.js
+++ textutils/src/components/Textform.js
@@ -39,10 +39,10 @@ export default function Textform(props) {
 
     return (
         <>
-        <div className = "container">
+        <div className = "container" style= {{color: props.mode==='light'?'dark':'light'}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" value = {text} onChange={handleOnChange} style={{backgroundColor: props.mode==='light'?'dark':'light'}} id="myBox" rows="8"></textarea>
+                <textarea className="form-control" value = {text} onChange={handleOnChange} style={{backgroundColor: props.mode==='light'?'dark':'light', color: props.mode==='light'?'dark':'light'}} id="myBox" rows="8"></textarea>
             </div>
             <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Upper Case </button>
             <button className="btn btn-primary mx-2" onClick={handleloClick}>Convert to Lower Case </button>
