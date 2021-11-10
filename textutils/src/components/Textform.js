eaf1ce678aec3585e49ec84150e441240b34bdb1
wasimiqbal1 <wasim_iqbal@msn.com>
2021-11-10 23:59:01 +0500

    line space update

--- textutils/src/components/Textform.js
+++ textutils/src/components/Textform.js
@@ -60,7 +60,7 @@ export default function Textform(props) {
         </div>
         <div className="container my-3" style= {{color: props.mode==='dark'?'white':'black'}}>
             <h2>Your Text Summary</h2>
-            <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} word & {text.length} characters</p>
+            <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} word & {text.length} characters</p>
             <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} minutes read</p>
             <h2>Preview</h2>
             <p>{text.length>0?text:"Nothing to preview!"}</p>
