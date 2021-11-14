9152853c306992ea3ac7ad58e390251a7fc7f3e8
wasimiqbal1 <wasim_iqbal@msn.com>
2021-11-14 23:29:27 +0500

    React class based app

---
+++ newsapp/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello my first Class based Component
+      </div>
+    );
+  }
+}
+
+export default App;
