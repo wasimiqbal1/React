2d298adc2343d8b1a767667fb55e9dc5cd31cea1
wasimiqbal1 <wasim_iqbal@msn.com>
2021-11-18 00:39:43 +0500

    props

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -8,7 +8,7 @@ class News extends Component {
                 <h2>News - Top Headlines</h2>
                 <div className="row">
                     <div className="col-md-4">
-                        <NewsItem title="myTitle" description="mydesc"/>
+                        <NewsItem title="myTitle" description="mydesc" imageUrl = "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/rockcms/2021-11/21116-washington-coast-guard-mb-1135-6def47.jpg"/>
                     </div>
                     <div className="col-md-4">
                         <NewsItem title="myTitle" description="mydesc"/>
--- newsapp/src/components/Newsitem.js
+++ newsapp/src/components/Newsitem.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export class Newsitem extends Component {
     render() {
-        let {title, description} = this.props;
+        let {title, description, imageUrl} = this.props;
         return (
             <div>
                 <div className="card" style={{width: "18rem"}}>
-                <img src="https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/rockcms/2021-11/21116-washington-coast-guard-mb-1135-6def47.jpg" className="card-img-top" alt="..."/>
+                <img src={imageUrl} className="card-img-top" alt="..."/>
                 <div className="card-body">
                     <h5 className="card-title">{title}</h5>
                     <p className="card-text">{description}</p>
-                    <a href="/Newsitem" className="btn btn-primary">Go somewhere</a>
+                    <a href="/Newsitem" className="btn btn-primary">Read More</a>
                 </div>
             </div>
         </div>
