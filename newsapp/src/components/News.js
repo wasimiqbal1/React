e685ec484e7b7fed187447bc2fe2f576ecef53e2
wasimiqbal1 <wasim_iqbal@msn.com>
2021-11-21 01:34:53 +0500

    Fetch Api

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -42,15 +42,25 @@ class News extends Component {
             loading: false
         }
     }
+
+    async componentDidMount(){
+        console.log("CDM")
+        let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=3f3168e5e3a8491c9485b6f0963bb15e";
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        console.log(parsedData);
+        this.setState({articles:parsedData.articles})
+    }
     
     render() {
+        console.log("render")
         return (
             <div className="container my-3">
                 <h2>News - Top Headlines</h2>
                 <div className="row">
                 {this.state.articles.map((element)=>{
                     return <div className="col-md-4" key = {element.url} >
-                    <NewsItem title={element.title.slice(0,45)} description={element.description.slice(0,88)} imageUrl = {element.urlToImage}
+                    <NewsItem title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,88):""} imageUrl = {element.urlToImage}
                     newsUrl= {element.url}/>
                 </div>
                 })}
--- newsapp/src/components/Newsitem.js
+++ newsapp/src/components/Newsitem.js
@@ -6,7 +6,7 @@ export class Newsitem extends Component {
         return (
             <div className="my-3">
                 <div className="card" style={{width: "18rem"}}>
-                <img src={imageUrl} className="card-img-top" alt="..."/>
+                <img src={!imageUrl?"https://i.cdn.newsbytesapp.com/images/l207_5971637399132.jpg":imageUrl} className="card-img-top" alt="..."/>
                 <div className="card-body">
                     <h5 className="card-title">{title}...</h5>
                     <p className="card-text">{description}...</p>
