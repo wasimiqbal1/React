b84f1b208922c5d97f1d80d3e98fdc7fe9e5f5b6
wasimiqbal1 <wasim_iqbal@msn.com>
2021-12-05 14:48:33 +0500

    Api Update

--- newsapp/src/components/Newsitem.js
+++ newsapp/src/components/Newsitem.js
@@ -6,11 +6,11 @@ export class Newsitem extends Component {
         return (
             <div className="my-3">
                 <div className="card" style={{width: "18rem"}}>
-                <img src={!imageUrl?"https://i.cdn.newsbytesapp.com/images/l207_5971637399132.jpg":imageUrl} className="card-img-top" alt="..."/>
+                <img src={!imageUrl?"https://i.cdn.newsbytesapp.com/images/l207_5971637399132.jpg":imageUrl} className="card-   img-top" alt="..."/>
                 <div className="card-body">
                     <h5 className="card-title">{title}</h5>
                     <p className="card-text">{description}</p>
-                    <a href={url} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+                    <a href={url} target="_blank" rel="noopener noreferrer" to="http://example.com" className="btn btn-sm btn-primary">Read More</a>
                 </div>
             </div>
         </div>
