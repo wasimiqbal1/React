b16e69d8e20a83e2200bf57978be254361607901
wasimiqbal1 <wasim_iqbal@msn.com>
2021-12-24 01:30:30 +0500

    Api Category Update

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -1,19 +1,29 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import News from './components/News';
-
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-class App extends Component {
-  
+export default class App extends Component {
+
   render() {
     return (
       <div>
+        <Router>
         <Navbar/>
-        <News pageSize={9} country="in" category="science" />
+          <Switch>
+          <Route exact path="/"><News key="general" pageSize={9} country="in" category="general"/></Route>
+          <Route exact path="/business"><News key="business" pageSize={9} country="in" category="business"/></Route>
+          <Route exact path="/entertainment"><News key="entertainment" pageSize={9} country="in" category="entertainment"/></Route>
+          <Route exact path="/health"><News key="health" pageSize={9} country="in" category="health"/></Route>
+          <Route exact path="/science"><News key="science" pageSize={9} country="in" category="science"/></Route>
+          <Route exact path="/sports"><News key="sports" pageSize={9} country="in" category="sports"/></Route>
+          <Route exact path="/technology"><News key="technology" pageSize={9} country="in" category="technology"/></Route>
+        </Switch>
+        </Router>
       </div>
     );
   }
 }
 
-export default App;
+ 
