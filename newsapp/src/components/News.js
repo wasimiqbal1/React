f8af81d8cb2e49845b9d5b4ae094a38f7fa75f0a
wasimiqbal1 <wasim_iqbal@msn.com>
2021-12-26 15:29:30 +0500

    Adding Infinite Scroll

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -102,4 +102,3 @@ class News extends Component {
 
 export default News;
 
-
--- newsapp/src/components/Newsitem.js
+++ newsapp/src/components/Newsitem.js
@@ -2,34 +2,21 @@ import React, { Component } from 'react'
 
 export class Newsitem extends Component {
     render() {
-        let { title, description, imageUrl, url, author, date, source } = this.props;
-        return ( <
-            div className = "my-3" >
-            <
-            div className = "card" >
-            <
-            span className = "position-absolute top-0 translate-middle badge rounded-pill bg-danger"
-            style = {
-                { left: '90%', zIndex: '1' } } > { source } <
-            /span> <
-            img src = {!imageUrl ? "https://cdn.mos.cms.futurecdn.net/MngwNVKYu9w9sYGws9RmC9-970-80.jpg" : imageUrl }
-            className = "card-img-top"
-            alt = "..." / >
-            <
-            div className = "card-body" >
-            <
-            h5 className = "card-title" > { title } < /h5> <
-            p className = "card-text" > { description } < /p> <
-            p className = "card-text" > < small className = "text-muted" > by {!author ? "Unknown" : author }
-            on { new Date(date).toDateString() } < /small></p >
-            <
-            a rel = "noreferrer"
-            href = { url }
-            target = "_blank"
-            className = "btn btn-sm btn-dark" > Read More < /a> <
-            /div> <
-            /div> <
-            /div>
+        let {title, description, imageUrl, url, author, date, source} = this.props;
+        return (
+            <div className="my-3">
+                <div className="card" >
+                <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex: '1'}}> {source}
+                        </span>
+                <img src={!imageUrl?"https://cdn.mos.cms.futurecdn.net/MngwNVKYu9w9sYGws9RmC9-970-80.jpg":imageUrl} className="card-img-top" alt="..."/>
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{description}</p>
+                    <p className="card-text"><small className="text-muted">by {!author?"Unknown":author} on {new Date(date).toDateString()}</small></p>
+                    <a rel="noreferrer" href={url} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+                </div>
+            </div>
+        </div>
         )
     }
 }
